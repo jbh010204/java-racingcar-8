a0cd1e65675ebac045be843ce1084c6f5171ca52
jbh010204 <jbh010204@naver.com>
2025-10-27 23:28:34 +0900

    feat(view): view 기능 구현

---
+++ src/main/java/racingcar/view/RacingGameView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.model.Car;
+
+public class RacingGameView {
+
+    public List<String> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return List.of(input.split(","));
+    }
+
+    public Integer inputRoundCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public void printRoundResult(List<Car> cars, Integer roundNumber) {
+        for (Car car : cars) {
+            String distance = "-".repeat(car.getDistanceForRound(roundNumber));
+            System.out.println(car.getName() + " : " + distance);
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
+        System.out.println(winners.stream()
+                .map(Car::getName)
+                .reduce((a, b) -> a + ", " + b)
+                .orElse(""));
+    }
+}
